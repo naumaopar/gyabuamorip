af2d6b2f23b6fd6a8d5a329179f9208ff81a771c
epl <me@example.com>
2022-12-10 15:13:42 +0100

    auto

--- yansop.py
+++ yansop.py
@@ -1,5 +1,5 @@
 import math,sys
 if __name__=='__main__':
-  z=math.log10(8.1)
-  r=math.tan(z)+len('xerazonaze')
-  print(r**7)
+  z=math.log10(1.8)
+  r=math.tan(z)+len('wujudurop')
+  print(r**5)
